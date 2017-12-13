e436918c48dca27a6cbdb8a041e2fb2fa7df4fe7
akv <mansho538552@gmail.com>
2017-12-13 20:46:28 +0900

    web.xml

--- EC/src/ec/UserBuyHistoryDetail.java
+++ EC/src/ec/UserBuyHistoryDetail.java
@@ -25,6 +25,7 @@ public class UserBuyHistoryDetail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		int buyId = Integer.parseInt(request.getParameter("buy_id"));
 		ArrayList<ItemDataBeans> buyDetailList = null;
 		try {
